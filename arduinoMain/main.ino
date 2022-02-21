03052e0745aa79028b4dc914da4c929a83438da3
kun <sus5330@icloud.com>
2022-02-22 02:36:46 +0900

    a

--- arduinoMain/main.ino
+++ arduinoMain/main.ino
@@ -2,15 +2,30 @@
 #include <Stepper.h>
 #include "HX711.h" 
 
+#define VACUUM_PIN 13
+#define EXHALE_PIN 7
+
+// loadcell_2
+#define LOADCELL_DOUT_PIN 2
+#define LOADCELL_SCK_PIN 3
+
+// loadcell_2
+#define LOADCELL_DOUT_PIN2 5 
+#define LOADCELL_SCK_PIN2 6 
+
+// ModuleServo
+#define MODULE_SERVO_PIN 8
+
+HX711 scale1;
+HX711 scale2;
+
 class customServo {
 private:
-// 멤버 변수
   int downAngle;
   int upAngle;
   Servo servo;
   
 public:
-
   customServo(int downAngle, int upAngle, Servo servo) {
     this->downAngle = downAngle;
     this->upAngle = upAngle;
@@ -34,10 +49,6 @@ class moveCartridge {
     int move_num;
 
   public:
-    // moveCartridge(int current_num) {
-    //   this -> current_num = current_num;
-    // }
-
     boolean move(int move_num) {
 
       this -> move_num = move_num;
@@ -52,9 +63,26 @@ class moveCartridge {
     }
 };
 
-void vacuum() {
-
+void vacuum(int pwm) {
+  analogWrite(VACUUM_PIN, pwm);
+  Serial.print("vacuum value : ");
+  Serial.println(pwm);
 }
+
+void exhale(int pwm) {
+  analogWrite(EXHALE_PIN, pwm);
+  Serial.print("exhale value : ");
+  Serial.println(pwm);
+}
+
+void coverMotor(String status) {
+  if (status == "open") {
+    Serial.println("coverMotor status : open");
+  } else {
+    Serial.println("coverMotor status : close");
+  }
+}
+
 
 class LoadCell {
   private:
@@ -62,6 +90,7 @@ class LoadCell {
     int calibration_factor2;
     HX711 scale1;
     HX711 scale2;
+
   public:
     LoadCell(HX711 scale1, HX711 scale2, int calibration_factor, int calibration_factor2) {
       this -> scale1 = scale1;
@@ -79,12 +108,11 @@ class LoadCell {
       scale1.set_scale(calibration_factor);
       scale2.set_scale(calibration_factor2);
       double sumWeight = scale1.get_units() + scale2.get_units();
-      Serial.println(sumWeight);
+      Serial.print(sumWeight);
+      Serial.println(" g");
       return sumWeight;
     }
-
-    
-};
+  };
 
 // double getWeight() {
 
@@ -93,24 +121,9 @@ class LoadCell {
   // return sumWeight;
 // }
 
-
-// loadcell_2
-#define LOADCELL_DOUT_PIN 2
-#define LOADCELL_SCK_PIN 3
-
-// loadcell_2
-#define LOADCELL_DOUT_PIN2 5 
-#define LOADCELL_SCK_PIN2 6 
-
-// ModuleServo
-#define MODULE_SERVO_PIN 8
-
-HX711 scale1; // hx-711 하나
-HX711 scale2; // hx-711 두개
-
-// 로드 셀 캘리브레이션
-int calibration_factor =-480;
-int calibration_factor2 =-480; // hx -711 두개를 사용 시 더 추가
+// LoadCell calibration
+int calibration_factor = -480;
+int calibration_factor2 = -480; // hx -711 두개를 사용 시 더 추가
 
 Stepper myStepper1(200, 12, 11, 10, 9);      
 Servo servo1;
@@ -118,23 +131,25 @@ Servo servo1;
 
 //////////////////////////////////////////////////////////////////////////////////
 void setup() {
-  // 시리얼 통신 설정
+  // Serial setting
   Serial.begin(19200);          
 
-  // 모듈 서보 설정
+  // Module servoMotor setting
   servo1.attach(MODULE_SERVO_PIN);
   pinMode(MODULE_SERVO_PIN, OUTPUT);
+  pinMode(EXHALE_PIN, OUTPUT);
+  pinMode(VACUUM_PIN, OUTPUT);
 
-  // 스텝 모터 설정
+  // StepMotor setting
   myStepper1.setSpeed(70); // 분당 스텝수 설정  --> 스텝모터의 속도를 결정한다. 
   
   
-  // 로드셀1 설정
+  // LoadCell1 setting
   scale1.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale1.set_scale();
   scale1.tare();
   
-  // 로드셀2 설정
+  // LoadCell2 setting
   scale2.begin(LOADCELL_DOUT_PIN2, LOADCELL_SCK_PIN2); 
   scale2.set_scale();
   scale2.tare();
@@ -145,90 +160,88 @@ void setup() {
 }
 
 // myStepper1.step(-200/6);
-// 반시계방향 회전 60도
+// turn counterclockwise 60 degrees.
 /*
  *
  *  1. 시리얼 입력이 있을때 까지 대기
  *  2. 입력정보 (n번 카트리지 m(g)출력 정보 6개) 해석
  *  3. 출력, 진행상황 시리얼 송신
  * 
+ *  현재 카트리지에서 n번째 카트리지로 이동하려면 [ -200/6 * (이동할 카트리지 번호 - 현재 카트리지 번호) ]
+ *  1 -> 2번 카트리지 -200/6 * 1 만큼 회전
+ *  1 -> 3번 카트리지 -200/6 * 2 만큼 회전 
+ *
  */
-/*
-
-현재 카트리지에서 n번째 카트리지로 이동하려면 [ -200/6 * (이동할 카트리지 번호 - 현재 카트리지 번호) ]
-1 -> 2번 카트리지 -200/6 * 1 만큼 회전
-1 -> 3번 카트리지 -200/6 * 2 만큼 회전 
-
-*/
 
 
 // 입력 개수 n개 
 // 3 -> 5 -> 6 번째 칸
-int n = 3;
-int Source_arr[3] = {3, 4, 6};
+
+// *** Implemented funtions ***
+// module servoMotor
+// shaft stepMotor
+// loadcell
+// vucuum, exhale motor
+// vibration motor
+// ----------------------------
+// cover servoMotor
+// 
 
 void loop() {
+
+  scale1.set_scale(calibration_factor);
+  scale2.set_scale(calibration_factor2);
   
   customServo moduleServo = customServo(70, 120, servo1);
   moveCartridge movecartridge ;
-  LoadCell loadcell = LoadCell(scale1, scale2, calibration_factor, calibration_factor2);
+  // LoadCell loadcell = LoadCell(scale1, scale2, calibration_factor, calibration_factor2);
 
   ////////////////////////// start /////////////////////////////////////////
-  scale1.set_scale(calibration_factor);
-  scale2.set_scale(calibration_factor2);
+
+  ///////////////// input value ///////////////////
+
+  int Source_arr[3] = {3, 4, 6};
+  int goal_weight[3] = {30, 10, 50};
+
+  int n = (int)(sizeof(Source_arr) / sizeof(int));
+
+  /////////////////////////////////////////////////
 
   for (int i = 0 ; i < n ; i++) {
     movecartridge.move(Source_arr[i]);
     delay(1000);
     moduleServo.down();
     delay(1000);
-    loadcell.tare();
-    while(loadcell.getWeight > 30){
-      loadcell.getWeight();
-    }
     
-    // double Weight = scale1.get_units() + scale2.get_units();
-    // Serial.println(Weight);
+    coverMotor("open");
+    exhale(255);
+
+    // reset
+    double Weight = 0;
+    scale1.tare();
+    scale2.tare();
+    Serial.print("Measuring the weight.");
+    while(Weight < goal_weight[i]) {
+       Weight = scale1.get_units() + scale2.get_units();
+      //  Serial.println(Weight);
+    }
+    Serial.println(Weight);
+    Serial.print("Measuring is over.");
+    vacuum(255);
+    exhale(0);
+
+    delay(1000);
+
+    coverMotor("close");
+    vacuum(0);
+
+    moduleServo.up();
+
     delay(1000);
   }
 
-
-/*
-  
-  scale1.set_scale(calibration_factor);
-  scale2.set_scale(calibration_factor2);
-
-  // print weight in serialmonitor
-  
-  Serial.print(" loadcell 1 : "); 
-  Serial.print(scale1.get_units(), 1);
-  Serial.print(" g");
-  double sumWeight = scale1.get_units() + scale2.get_units();
-  Serial.print(" loadcell 2 : ");
-  Serial.print(scale2.get_units(), 1);
-  Serial.println(" g"); 
-
-  Serial.print(sumWeight);
-  Serial.println(" g"); 
-  
-*/
-
-//   if (sumWeight > 28 && sumWeight < 57) {
-//     
-//     delay(500);
-//   } else if (sumWeight > 57) {
-//     myStepper1.step(-200/62);
-//     delay(500);
-//   }
-
-
-/*
-  if(sumWeight > 5) {
-    moduleServo.up();
-  } else {
-      moduleServo.down();
-  }
-  */
+  Serial.println("End cycle");
+  delay(5000);
 
 }
 
